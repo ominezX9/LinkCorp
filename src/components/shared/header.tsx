3fab208aaef51f217111166969c64814bef792cc
ominezx9 <68435491+ominezX9@users.noreply.github.com>
2024-10-10 21:52:31 +0000

    add: chakra ui and framer motion

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <div className=''>
+                <div className="logo">
+                    <img src="" alt="" />
+                </div>
+                
+            </div>
+            <div className=''>
+
+            </div>
+        </header>
+    )
+}
